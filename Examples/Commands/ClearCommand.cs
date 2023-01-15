111a607ad0b71b928ce89a0817385e627fa6d89d
Polish Coder <polygonssoftware@gmail.com>
2023-01-15 13:26:33 +0100

    Added sample Clear command

---
+++ Examples/Commands/ClearCommand.cs
@@ -0,0 +1,12 @@
+﻿using InGameConsole;
+
+public class ClearCommand : Command
+{
+    public override string Name => "clear";
+    public override string Description => "Clears console output";
+    
+    public override void Execute(string[] args)
+    {
+        Console.Clear();
+    }
+}
